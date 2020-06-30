7c7c6c8f6af3832f497a3951bcdf928ff564df2a
Michelle <mysdmg312@gmail.com>
2020-06-30 12:49:30 -0700

    Add form text input and dropdown menu elements

--- src/components/AddNewPartForm.jsx
+++ src/components/AddNewPartForm.jsx
@@ -5,7 +5,27 @@ function AddNewPartForm(props) {
   if (props.isAddingPart === true) {
     return (
       <div>
-        hi i'm a div-a
+        <form>
+          <label>
+            URL:
+            <input type="text"></input>
+          </label>
+          <br />
+          <label>
+            Vendor:
+            <select>
+              <option value="cdw">CDW</option>
+              <option value="newegg">Newegg</option>
+            </select>
+          </label>
+          <br />
+          <label>
+            Category:
+            <select>
+              <option value="cables">Cables</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   } else {
